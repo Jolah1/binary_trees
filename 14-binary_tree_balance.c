8dbc7c60fe9532ec48399f53e1b30110cc74e973
jolah1 <jokunlade@gmail.com>
2022-09-22 02:25:47 -0700

    tree balance corrected

--- 14-binary_tree_balance.c
+++ 14-binary_tree_balance.c
@@ -23,6 +23,7 @@ size_t binary_tree_height(const binary_tree_t *tree)
 	if (tree)
 	{
 		size_t l = 0, r = 0;
+
 		l = tree->left ? 1 + binary_tree_height(tree->left) : 1;
 		r = tree->right ? 1 + binary_tree_height(tree->right) : 1;
 		return ((l > r) ? l : r);
